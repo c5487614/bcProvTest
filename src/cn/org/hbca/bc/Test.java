fd901bbc967c52814908548da684e7c6bd1398bf
MarkChen <xway89@126.com>
2016-05-30 22:02:20 +0800

    AES

--- src/cn/org/hbca/bc/Test.java
+++ src/cn/org/hbca/bc/Test.java
@@ -12,6 +12,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.Provider;
+import java.security.PublicKey;
 import java.security.Security;
 import java.security.Signature;
 import java.security.SignatureException;
@@ -24,6 +25,12 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Set;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.bouncycastle.asn1.ASN1Enumerated;
 import org.bouncycastle.asn1.ASN1InputStream;
@@ -50,6 +57,7 @@ import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.SHA1Digest;
 import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.jcajce.provider.symmetric.AES.KeyGen;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
@@ -60,59 +68,39 @@ import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
 import org.bouncycastle.util.Store;
 import org.bouncycastle.util.Strings;
 import org.bouncycastle.util.encoders.Base64;
+import org.bouncycastle.util.encoders.Base64Encoder;
 
 public class Test {
 
-	public static void main(String[] args) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, FileNotFoundException, IOException, UnrecoverableKeyException, OperatorCreationException, CMSException, NoSuchProviderException, SignatureException, InvalidKeyException {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		//signDataVerify();
+		byte[] myIV = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		byte[] keys = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+		Security.addProvider(new BouncyCastleProvider());
+		KeyGenerator kg = KeyGenerator.getInstance("AES","BC");
+		kg.init(256);
+		
+		SecretKey secretKey=new SecretKeySpec(keys,"AES");
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey,new IvParameterSpec(myIV));
+		
+		byte[] encryptData = cipher.doFinal("HBCA20160530".getBytes());
+		Base64 base64 = new Base64();
+		System.out.println(new String(base64.encode(encryptData)));
+		
+		cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
+		cipher.init(Cipher.DECRYPT_MODE, secretKey,new IvParameterSpec(myIV));
+		byte[] data = cipher.doFinal(encryptData);
+		System.out.println(new String(data));
+		
+	}
+	private static void asn1Explain() throws IOException{
 		ASN1InputStream ais = new ASN1InputStream(new FileInputStream(new File("F:/cch.cer")));
 	    ASN1Sequence asnSeq = (ASN1Sequence) ais.readObject();
 	    Enumeration enum1 = asnSeq.getObjects();
 	    Util.enumASN1Object(enum1);
-	}
-	private static void asn1Explain() throws IOException{
-		String data = "MIIDyTCCAzKgAwIBAgIQFMmwMc+3V8BIwCaNTu2MBjANBgkqhkiG9w0BAQUFADB5MQswCQYDVQQGEwJDTjEOMAwGA1UECAwFSFVCRUkxDjAMBgNVBAcMBVdVSEFOMTswOQYDVQQKDDJIdWlCZWkgRGlnaXRhbCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkgQ2VudGVyIENPLkxURDENMAsGA1UEAwwESEJDQTAeFw0xNjA0MjUwODAwMTRaFw0xNzA0MjUwODAwMTRaMFAxCzAJBgNVBAYTAkNOMQ8wDQYDVQQIDAbmuZbljJcxDzANBgNVBAcMBumaj+W3njEfMB0GA1UEAwwW6ZmI5pil6L6J5py65p6E5rWL6K+VMTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwhcdmA2w1WVdef5+ZspB+n2TJgv3DcuV/2d7BChed62C1+kYfW42o5mV7IwIP+RTgYoTgfqekAzf2fRTPJAN75QPK76XjC9ykKVLVwNLooqbpjYlnwLJqR3ymF4QgyM+ZgGq4TcWSxRJMRY8OFeOgwN/viq6uoRvrhFM0hk/5QECAwEAAaOCAXkwggF1MB8GA1UdIwQYMBaAFFt6x6jV3alaHcJPyZOgFeRrksQ7MAwGA1UdEwQFMAMBAQAwEAYFVBALBwEEBwwFKjYzQiowgfAGA1UdHwSB6DCB5TA2oDSgMqQwMC4xCzAJBgNVBAYTAkNOMRAwDgYDVQQLDAdBREQxQ1JMMQ0wCwYDVQQDDARjcmw5MDGgL6AthitodHRwOi8vd3d3LmhiY2Eub3JnLmNuL2NybF9yc2ExMDI0L2NybDkuY3JsMHigdqB0hnJsZGFwOi8vMjIxLjIzMi4yMjQuNzQ6MTM4OS9DTj1jcmw5LE9VPUFERDFDUkwsQz1DTj9jZXJ0aWZpY2F0ZVJldm9jYXRpb25MaXN0P2Jhc2U/b2JqZWN0Y2xhc3M9Y1JMRGlzdHJpYnV0aW9uUG9pbnQwEwYFKlYLBwMEChMINDQ0NDQ0NDQwCwYDVR0PBAQDAgbAMB0GA1UdDgQWBBSkefovysActwRgX2J+zU3RoZ4EcDANBgkqhkiG9w0BAQUFAAOBgQAFcMvWJcU4DChyfMVciJr3qMOTQxS2sXYYb1m5ob9M8OLdPEBH6Sr6mq3Jjez3K0IWBERr4bGXLefD5s860ra2tqOupV7k51Ceg/2gc2ybiChX41F8/txshFCMTTpe4YC7rQU0Wz8fxA8m/wzy0xWoKAGC9ikt40cpuNo9xP1GTg==";
-		ByteArrayInputStream inStream = new ByteArrayInputStream(data.getBytes());
-	    ASN1InputStream asnInputStream = new ASN1InputStream(inStream);
-//	    DERApplicationSpecific derApp = (DERApplicationSpecific)asnInputStream.readObject();
-//	    DERGeneralString asn1Enum = (DERGeneralString) derApp.getObject(BERTags.GENERAL_STRING);
-//	    Base64.decode()
-	    
-//	    ASN1InputStream ais = new ASN1InputStream(new FileInputStream(new File("F:/cch.cer")));
-//	    while (ais.available() > 0) {
-//	        ASN1Primitive obj = ais.readObject();
-//	        System.out.println(ASN1Dump.dumpAsString(obj, true));
-//	    }
-	    
-	    ASN1InputStream ais = new ASN1InputStream(new FileInputStream(new File("F:/cch.cer")));
-	    ASN1Sequence asnSeq = (ASN1Sequence) ais.readObject();
-	    Enumeration enum1 = asnSeq.getObjects();
-	    while(enum1.hasMoreElements()){
-	    	System.out.println("i");
-	    	Object obj = enum1.nextElement();
-	    	if(obj instanceof ASN1Sequence){
-	    		ASN1Sequence asn1Seq = (ASN1Sequence)obj;
-	    		Enumeration enum2 = asn1Seq.getObjects();
-	    		while(enum2.hasMoreElements()){
-	    			Object obj2 = enum2.nextElement();
-	    			if(obj2 instanceof ASN1Sequence){
-	    				ASN1Sequence asn1Seq2 = (ASN1Sequence)obj2;
-	    				Enumeration enum3 = asn1Seq2.getObjects();
-	    				while(enum3.hasMoreElements()){
-	    					Object obj3 = enum3.nextElement();
-	    					if(obj3 instanceof ASN1ObjectIdentifier){
-	    						ASN1ObjectIdentifier asn1ObjectIndentifier = (ASN1ObjectIdentifier)obj3;
-	    						String str = new String(asn1ObjectIndentifier.getId());
-	    						System.out.println(str);
-	    					}
-	    				}
-	    			}
-	    		}
-	    	}else{
-	    		
-	    	}
-	    }
-	    inStream.close();
+	    ais.close();
 	}
 	private static void readCert() throws FileNotFoundException, CertificateException{
 		FileInputStream fis=new FileInputStream("F:/cch.cer");
@@ -127,6 +115,39 @@ public class Test {
 		 }
 
 		System.out.println(cert.getSubjectDN().getName());
+	}
+	private static void signDataVerify() throws NoSuchAlgorithmException, NoSuchProviderException, CertificateException, FileNotFoundException, IOException, Exception{
+		Provider provider = new BouncyCastleProvider();
+		Security.addProvider(provider);
+		String data = "ChunhuiChen";
+		KeyStore ks = KeyStore.getInstance("PKCS12");
+		ks.load(new FileInputStream("F:/十堰机构测试_签名.pfx"), "11111111".toCharArray());
+		Enumeration enums = ks.aliases();
+		while(enums.hasMoreElements()){
+			String keyAlias = (String) enums.nextElement();  
+			System.out.println("alias=[" + keyAlias + "]");  
+			if(ks.isKeyEntry(keyAlias)){
+				System.out.println("isKeyEntry=[" + keyAlias + "]");
+				PrivateKey prikey=(PrivateKey)ks.getKey(keyAlias, "11111111".toCharArray());
+				
+				Signature signature = Signature.getInstance("SHA1WithRSA", "BC");
+				signature.initSign(prikey);
+		        signature.update(data.getBytes());
+		        byte[] signedResult = signature.sign();
+		        System.out.println(new String(Base64.encode(signedResult)));  
+				
+		        PublicKey pubKey = ks.getCertificate(keyAlias).getPublicKey();
+		        signature.initVerify(pubKey);
+		        signature.update(data.getBytes());
+		        boolean bSucc = signature.verify(signedResult);
+		        System.out.println(bSucc);
+		        
+			}
+			if(ks.isCertificateEntry(keyAlias)){
+				System.out.println("isCertificateEntry=[" + keyAlias + "]");  
+			}
+		}
+		
 	}
 	private static void p7Sign() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, FileNotFoundException, IOException, UnrecoverableKeyException, NoSuchProviderException, InvalidKeyException, OperatorCreationException, CMSException, SignatureException{
 		Provider provider = new BouncyCastleProvider();
